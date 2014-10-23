be6573be1908000726da26fe89d04715ea48b1e7
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-10-23 16:20:44 -0500

    if statements in correct place now

--- app/assets/javascripts/raffle.js
+++ app/assets/javascripts/raffle.js
@@ -1,39 +1,6 @@
-// var app = angular.module('myApp',["ngResource"]);
-
-// app.factory("entry", function($resource) {
-//     $resource("/entries/:id", {id: "@id"}, {update: {method: "PUT"}});
-//   });
-
-// app.controller('raffleCtrl', function($scope, $resource){
-//   $scope.entries = [
-//     {name: 'Bob'},
-//     {name: 'Juan'},
-//     {name: 'Tad'}
-//     ]
-//   $scope.addEntry = function(){
-//     $scope.entries.push($scope.newEntry)
-//     $scope.newEntry = {}
-//   };
-
-//   $scope.drawWinner = function(){
-//     pool = []
-//     angular.forEach($scope.entries, function(entry){
-//       if (!entry.winner) {
-//         pool.push(entry) 
-//       }
-//     })
-//     if (pool.length > 0) {
-//       var index = Math.floor(Math.random()*pool.length)
-//       entry = pool[index];
-//       entry.winner = true;
-//       $scope.lastWinner = entry;
-//     }
-//   }
-// })
-
 var app = angular.module("myApp", ["ngResource"])
 
-app.factory ("Entry", ["$resource", function($resource){
+app.factory("Entry", ["$resource", function($resource){
   return $resource("/entries/:id", {id: "@id"}, {update: {method: "PUT"}})}
 ])
 
@@ -47,15 +14,19 @@ app.controller('raffleCtrl', ["$scope", "Entry", function($scope, Entry){
   }
     
   $scope.drawWinner = function(){
-    pool = []
+    var pool = []
     angular.forEach($scope.entries, function(entry){
-      if (!entry.winner){pool.push(entry)}
-      if (pool.length > 0){
-      entry = pool[Math.floor(Math.random()*pool.length)]
-        entry.winner = true
-        entry.$update()
-        $scope.lastWinner = entry
+      if (!entry.winner){
+        console.log("Hi")
+        pool.push(entry)
       }
     })
+
+    if (pool.length > 0){
+    entry = pool[Math.floor(Math.random()*pool.length)]
+      entry.winner = true
+      entry.$update()
+      $scope.lastWinner = entry
+    }
   }
 }])
